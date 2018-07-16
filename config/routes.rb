ecd967e69f35a2a5d0c68d969e93d29ff121ea1f
Christophe Junier <christophejunier@gmail.com>
2018-07-16 18:02:08 +0200

    configuring style_guide home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ActiveAdmin.routes(self)
+  
+  root 'home#index'
 end
