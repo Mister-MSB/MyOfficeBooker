9962178226f8e965f91e7c4d04be49224385b603
Christophe Junier <christophejunier@gmail.com>
2018-08-31 10:32:28 +0200

    add owner full

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
+  before_action :check_redirection, only: %w(index)
+
   layout "home"
 
   def index
+  end
+
+  private
+
+  def check_redirection
+    if current_owner.present?
+      redirect_to owners_home_path
+    end
   end
 
 end
