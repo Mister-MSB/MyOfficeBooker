5cda234a9b100b0251f82e588bd4f2e6cce8fdc2
Christophe Junier <christophejunier@gmail.com>
2018-08-31 09:29:46 +0200

    adding owner route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :bookers,
     controllers: {passwords: "bookers/passwords", registrations: "bookers/registrations", sessions: "bookers/sessions"},
-    path_names: { sign_in: 'connexion', sign_out: 'deconnexion', sign_up: 'inscription'}
+    path_names: { sign_in: 'booker/connexion', sign_out: 'booker/deconnexion', sign_up: 'booker/inscription'}
+
+  devise_for :owners,
+    controllers: {passwords: "owners/passwords", registrations: "owners/registrations", sessions: "owners/sessions"},
+    path_names: { sign_in: 'owner/connexion', sign_out: 'owner/deconnexion', sign_up: 'owner/inscription'}
+
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
