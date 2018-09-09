d5fd79d8fd320c3ccf33b074964b81d7d405f646
Jimmy FRAGNE <fragnejimmy@gmail.com>
2018-09-09 16:22:36 +0200

    my profil page

--- app/assets/javascripts/all_js.js
+++ app/assets/javascripts/all_js.js
@@ -32,3 +32,21 @@
 
     var current = document.getElementsByClassName("active");
   }
+
+
+//tabs my account
+  function tabsaccount(evt, useName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent_account");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks_account");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(useName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    var current = document.getElementsByClassName("active");
+  }
