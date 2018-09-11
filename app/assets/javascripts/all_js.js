a0c9e9f278afa44f113ee2228536ec629e44d4d2
Jimmy FRAGNE <fragnejimmy@gmail.com>
2018-09-11 10:57:07 +0200

    fix header price

--- app/assets/javascripts/all_js.js
+++ app/assets/javascripts/all_js.js
@@ -50,3 +50,32 @@
 
     var current = document.getElementsByClassName("active");
   }
+
+
+////// pop up  //////
+$(function() {
+
+  // Vars
+  var modBtn  = $('#modBtn'),
+      modal   = $('#modal'),
+      close   = modal.find('.close'),
+      modContent = modal.find('.modal-content');
+
+  // open modal when click on open modal button
+  modBtn.on('click', function() {
+    modal.css('display', 'block');
+    modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+  });
+
+  // close modal when click on close button or somewhere out the modal content
+  $(document).on('click', function(e) {
+    var target = $(e.target);
+    if(target.is(modal) || target.is(close)) {
+      modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+        modal.css('display', 'none');
+        next();
+      });
+    }
+  });
+
+});
