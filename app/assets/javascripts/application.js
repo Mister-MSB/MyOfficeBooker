c2b35e9f404642b46dfe8c00ff81b099a0194eb7
Christophe Junier <christophejunier@gmail.com>
2018-09-29 17:10:25 +0200

    updating card to be cooler

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
+//= require jquery.slick
 //= require activestorage
 //= require turbolinks
-//= require_tree .
