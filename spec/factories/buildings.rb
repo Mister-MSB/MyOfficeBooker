4e15a26cdf9059eac9f45f23d687b34e38b1a3b5
Christophe Junier <christophejunier@gmail.com>
2018-11-18 12:05:49 +0100

    adding gmaps and finihsing design

--- spec/factories/buildings.rb
+++ spec/factories/buildings.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :building do
     name    "Hotel Concorde"
-    lat     0.25
-    long    0.25
+    lat     0
+    long    0
     street  "11 rue de la paix"
     zipcode "69000"
     city    "Lyon"
